9ded19686eb6eb730f1e88fe6b4df2e33469a397
Antoine Chérier <a.cherier@student.isartdigital.com>
2019-01-26 16:36:15 -0500

    Add MainLevel Scene (Blockout + Respawn system)

---
+++ Assets/Scripts/Respawn.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+	public GameObject player;
+	public GameObject checkpoint;
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		if (collision.collider.tag == "Player")
+		{
+			player.transform.position = checkpoint.transform.position;
+			Debug.Log("TEST");
+		}
+	}
+
+}
