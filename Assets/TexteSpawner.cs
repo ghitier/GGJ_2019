b8bcd068b8aa49f30bc0468663c1b38e57464c1d
5odrek <a.tab@student.isartdigital.com>
2019-01-27 09:16:48 -0500

    TextSpawn Script

---
+++ Assets/TexteSpawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TexteSpawner : MonoBehaviour
+{
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("SpawnSentences");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    IEnumerator SpawnSentences()
+    {
+        while (true)
+        {
+            GameObject newGO = new GameObject("myTextGO");
+            newGO.transform.SetParent(this.transform);
+
+            Text myText = newGO.AddComponent<Text>();
+            myText.text = "" + GameManager.instance.ballData[Random.Range(0, GameManager.instance.ballData.Count)];
+            yield return new WaitForSeconds(Random.Range(2f, 5f));
+        }
+        
+    }
+}
